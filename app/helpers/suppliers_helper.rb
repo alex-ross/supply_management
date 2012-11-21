8d4ac946996b6046c556a3e672cf4eba75792355
Alexander Ross <izepax@gmail.com>
2012-11-21 14:29:49 +0100

    Edited distance helper

--- app/helpers/suppliers_helper.rb
+++ app/helpers/suppliers_helper.rb
@@ -1,8 +1,8 @@
 module SuppliersHelper
   def distance(object_from, object_to)
     dest = Gmaps4rails.destination({from: object_from.gmaps4rails_address, to: object_to.gmaps4rails_address})
-    "#{dest.first["distance"]["text"]} from your store!"
+    "#{dest.first["distance"]["text"]} from you!"
   rescue
-    ""
+    "Your adress or suppliers address can't be found!"
   end
 end
